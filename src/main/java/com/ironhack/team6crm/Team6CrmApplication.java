bf707fd7b51342911fd4ada1b96a5f133b312b63
o-andres-m <elhinchon@gmail.com>
2022-11-19 16:08:12 +0100

    :rocket:

---
+++ src/main/java/com/ironhack/team6crm/Team6CrmApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.team6crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team6CrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team6CrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ironhack/team6crm/Team6CrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.team6crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team6CrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
