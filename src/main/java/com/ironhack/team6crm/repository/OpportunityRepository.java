0ebc13cf1c3bbbf6710fe0292b4708ffe18baf08
Pau Tusell <ptapfel@gmail.com>
2022-12-03 16:09:44 +0100

    MenuReport v1

--- src/main/java/com/ironhack/team6crm/repository/OpportunityRepository.java
+++ src/main/java/com/ironhack/team6crm/repository/OpportunityRepository.java
@@ -2,8 +2,32 @@ package com.ironhack.team6crm.repository;
 
 import com.ironhack.team6crm.model.Opportunity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OpportunityRepository extends JpaRepository<Opportunity,Long> {
+
+    @Query(value = "SELECT t.sales_rep_id, COUNT(t.id) FROM opportunity AS t GROUP BY t.sales_rep_id", nativeQuery = true)
+    List<Object[]> countBySalesRep();
+
+    @Query(value = "SELECT t.product, COUNT(t.id) FROM opportunity AS t GROUP BY t.product", nativeQuery = true)
+    List<Object[]> countByProduct();
+
+    @Query(value = "SELECT a.city, COUNT(t.id) FROM opportunity t inner join account a on t.account_id = a.id GROUP BY a.city", nativeQuery = true)
+    List<Object[]> countByCity();
+
+    @Query(value = "SELECT a.country, COUNT(t.id) FROM opportunity t inner join account a on t.account_id = a.id GROUP BY a.country", nativeQuery = true)
+    List<Object[]> countByCountry();
+
+    @Query(value = "SELECT a.industry, COUNT(t.id) FROM opportunity t inner join account a on t.account_id = a.id GROUP BY a.industry", nativeQuery = true)
+    List<Object[]> countByIndustry();
+    @Query(value = "SELECT t.sales_rep_id, COUNT(t.id) FROM opportunity AS t where status = 'close-won' GROUP BY t.sales_rep_id", nativeQuery = true)
+    List<Object[]> countCloseWonBySalesRep();
+
+    @Query(value = "SELECT t.product, COUNT(t.id) FROM opportunity AS t where status = 'close-won' GROUP BY t.product", nativeQuery = true)
+    List<Object[]> countCloseWonByProduct();
+
 }
--- src/main/java/com/ironhack/team6crm/service/MenuReport.java
+++ src/main/java/com/ironhack/team6crm/service/MenuReport.java
@@ -1,19 +1,104 @@
 package com.ironhack.team6crm.service;
 
+import com.ironhack.team6crm.repository.AccountRepository;
+import com.ironhack.team6crm.repository.ContactRepository;
+import com.ironhack.team6crm.repository.LeadRepository;
+import com.ironhack.team6crm.repository.OpportunityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MenuReport {
-    public static void reportsMenu(String[] options){
+
+    @Autowired
+    LeadRepository leadRepository;
+    @Autowired
+    OpportunityRepository opportunityRepository;
+    @Autowired
+    AccountRepository accountRepository;
+    @Autowired
+    ContactRepository contactRepository;
+
+    public void reportsMenu(String[] options){
 
         switch (options[1]){
             case "lead":{
-                System.out.println("Report lead");
+                var target = "Lead";
+                if (options[3].equals("salesrep")) {
+                    var segment = "SalesRep";
+                    System.out.printf("Report: %s by %s",target,segment);
+                    var results = leadRepository.countBySalesRep();
+                    printReportResult(results);
+                } else System.out.println("Invalid input");
                 break;
             }
             case "opportunity":{
-                System.out.println("Report opportunity");
+                var target = "Opportunity";
+                switch (options[3]) {
+                    case "salesrep": {
+                        var segment = "SalesRep";
+                        System.out.printf("Report: %s by %s",target,segment);
+                        var results = opportunityRepository.countBySalesRep();
+                        printReportResult(results);
+                        break;
+                    }
+                    case "product": {
+                        var segment = "Product";
+                        System.out.printf("Report: %s by %s",target,segment);
+                        var results = opportunityRepository.countByProduct();
+                        printReportResult(results);
+                        break;
+                    }
+                    case "city": {
+                        var segment = "City";
+                        System.out.printf("Report: %s by %s",target,segment);
+                        var results = opportunityRepository.countByCity();
+                        printReportResult(results);
+                        break;
+                    }
+                    case "country": {
+                        var segment = "Country";
+                        System.out.printf("Report: %s by %s",target,segment);
+                        var results = opportunityRepository.countByCountry();
+                        printReportResult(results);
+                        break;
+                    }
+                    case "industry": {
+                        var segment = "Industry";
+                        System.out.printf("Report: %s by %s",target,segment);
+                        var results = opportunityRepository.countByIndustry();
+                        printReportResult(results);
+                        break;
+                    }
+                }
                 break;
             }
             case "close-won":{
-                System.out.println("Report close-won");
+                var target = "Opportunities Close-Won";
+                switch (options[3]) {
+                    case "salesrep": {
+                        var segment = "SalesRep";
+                        System.out.printf("Report: %s by %s",target,segment);
+                        var results = opportunityRepository.countCloseWonBySalesRep();
+                        printReportResult(results);
+                        break;
+                    }
+                    case "product": {
+                        var segment = "Product";
+                        System.out.printf("Report: %s by %s",target,segment);
+                        var results = opportunityRepository.countCloseWonByProduct();
+                        printReportResult(results);
+                        break;
+                    }
+                    case "city": {
+                    }
+                    case "country": {
+                    }
+                    case "industry": {
+                    }
+                }
                 break;
             }
             case "close-lost":{
@@ -31,6 +116,14 @@ public class MenuReport {
             default:{
                 System.out.println("Need More actions!");
             }
+        }
+    }
+
+    private static void printReportResult(List<Object[]> results) {
+        for (Object[] result : results) {
+            String name = (String) result[0];
+            int count = ((Number) result[1]).intValue();
+            System.out.println(name + ": " + count);
         }
     }
 }
